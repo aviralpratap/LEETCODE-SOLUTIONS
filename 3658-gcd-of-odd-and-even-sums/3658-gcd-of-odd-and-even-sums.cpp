ea123952a9792509c61ed12a4053097aaca4ea1a
aviralpratap <aviralthakur110022@gmail.com>
2026-08-26 01:01:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (10.19%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int oddsum=0;
+        int evensum=0;
+        for(int i=1;i<2*n;i++){
+            if(i%2!=0) oddsum+=i;
+            else evensum+=i;
+        }
+         while(evensum!=0) {
+            int rem=oddsum%evensum;
+            oddsum=evensum;
+            evensum=rem;
+        }
+
+        return oddsum;
     }
 };
