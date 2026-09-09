8a50b7d49319e8dcc1b3faa3e1d799b9e4268237
aviralpratap <aviralthakur110022@gmail.com>
2026-09-09 22:34:16 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.33%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if(n<1000){
+            return 0;
+        }
+        else if(n>=1000 && n<1000000){
+            return n-999;
+        }
+        else if(1000000<=n && n<1000000000){
+            return n-999+n-999999;
+        }
+        else if(n>=1000000000 && n<1000000000000){
+            return n-999+n-999999+n-999999999;
+        }
+        else if(n>=1000000000000 && n<1000000000000000){
+            return n-999+n-999999+n-999999999+n-999999999999;
+        }
+        else{
+            return n-999+n-999999+n-999999999+n-999999999999+1;
+
+        }
+        
+    }
+};
